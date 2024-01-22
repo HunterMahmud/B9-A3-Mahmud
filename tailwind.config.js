835265e340d487c4913e066682f79b3f4e0b35f4
huntermahmud <mahmudthedatahunter@gmail.com>
2024-01-22 23:33:07 +0600

    professional hockeys section with responsive completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [],
+    content: ["*"],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          'manrope': ["Manrope"],
+        },
+      },
     },
     plugins: [],
   }
